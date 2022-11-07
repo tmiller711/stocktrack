55801582058140d473b9caeceb26fbf21ade61ef
Tristin <tmille2004@gmail.com>
2022-11-06 22:32:09 -0600

    api stores stock data

--- api/main/urls.py
+++ api/main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GetStockData
 
 urlpatterns = [
-    path('api/<str:tikr>', GetStockData.as_view())
+    path('<str:tikr>/', GetStockData.as_view())
 ]
--- api/main/views.py
+++ api/main/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.views import APIView
+from rest_framework.response import Response
+import pandas as pd
+
+from .models import Stock
+from .serializers import StockSerializer
 
 # Create your views here.
 
 class GetStockData(APIView):
-    def post(self, request, *args, **kwargs):
-        tikr = self.kwargs['tikr']
+    def get(self, request, *args, **kwargs):
+        ticker = kwargs['tikr']
+        try:
+            stock = Stock.objects.get(ticker=ticker)
+        except:
+            return Response({"Error": "Stock not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        data = pd.read_csv(stock.stock_data)
+        print(data)
+        print(request.data)
+        return Response()
