314741b1c9e20e73d468452103e76f2d96cbe1e6
Tristin <tmille2004@gmail.com>
2022-11-11 17:27:22 -0600

    adds some tests

--- tests/stocktrack_backtest_test.py
+++ tests/stocktrack_backtest_test.py
@@ -1,8 +1,16 @@
 import unittest
 from click.testing import CliRunner
 from cli.stocktrack import main
+import pathlib
 
 class StockTrackTests(unittest.TestCase):
+    def test_dirs(self):
+        runner = CliRunner()
+        path = pathlib.Path(__file__).parent.resolve()
+        print(path)
+        result = runner.invoke(main, ['setdir'], input=f"{path}\n{path}")
+        self.assertEqual(result.exit_code, 0)
+
     def test_create(self):
         runner = CliRunner()
         result = runner.invoke(main, ['create', 'test'])
@@ -15,22 +23,22 @@ class StockTrackTests(unittest.TestCase):
         self.assertEqual(result.exit_code, 0)
 
     # figure out how to exit graph
-    # def test_run_no_out(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['run'], input='test', )
-    #     runner.invoke(main, input='10')
-    #     self.assertEqual(result.exit_code, 0)
-
-    # def test_run_out(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['run', '-o test'], input='test\n10')
-    #     self.assertEqual(result.exit_code, 0)
-
-    def test_delete_notexist(self):
+    def test_run_no_out(self):
         runner = CliRunner()
-        result = runner.invoke(main, ['delete'], input='not_exist')
-        assert "Error deleting not_exist" in result.output
+        result = runner.invoke(main, ['run'], input='test/n10')
+        runner.invoke(main, input='10')
         self.assertEqual(result.exit_code, 0)
+
+    def test_run_out(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ['run', '-o test'], input='test\n10')
+        self.assertEqual(result.exit_code, 0)
+
+    # def test_delete_notexist(self):
+    #     runner = CliRunner()
+    #     result = runner.invoke(main, ['delete'], input='not_exist')
+    #     assert "Error deleting not_exist" in result.output
+    #     self.assertEqual(result.exit_code, 0)
 
     # def test_delete_exist(self):
     #     runner = CliRunner()
