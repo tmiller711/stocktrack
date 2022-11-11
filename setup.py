c99cac369c8604a3bda2d357822d461155f150f9
Tristin <tmille2004@gmail.com>
2022-11-10 23:47:38 -0600

    adds long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages 
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='stocktrack',
     version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'click',
