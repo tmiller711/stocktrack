39cae8a25ec298164a1f8b9f4aee4b909fea9d59
Tristin <tmille2004@gmail.com>
2022-11-11 00:12:50 -0600

    testing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='stocktrack',
-    version='0.0.2',
+    version='0.0.3',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
