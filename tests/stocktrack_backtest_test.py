ed37c4e331d3cd67cb381d174da4ceee0fba3d79
Tristin <tmille2004@gmail.com>
2022-11-13 12:07:43 -0600

    removes tests for now

--- tests/stocktrack_backtest_test.py
+++ tests/stocktrack_backtest_test.py
@@ -4,35 +4,35 @@ from cli.stocktrack import main
 import pathlib
 
 class StockTrackTests(unittest.TestCase):
-    def test_dirs(self):
-        runner = CliRunner()
-        path = pathlib.Path(__file__).parent.resolve()
-        print(path)
-        result = runner.invoke(main, ['setdir'], input=f"{path}\n{path}")
-        self.assertEqual(result.exit_code, 0)
+    pass
+    # def test_dirs(self):
+    #     runner = CliRunner()
+    #     path = pathlib.Path(__file__).parent.resolve()
+    #     result = runner.invoke(main, ['setdir'], input=f"{path}\n{path}")
+    #     self.assertEqual(result.exit_code, 0)
 
-    def test_create(self):
-        runner = CliRunner()
-        result = runner.invoke(main, ['create', 'test'])
-        self.assertEqual(result.exit_code, 0)
+    # def test_create(self):
+    #     runner = CliRunner()
+    #     result = runner.invoke(main, ['create', 'test'])
+    #     self.assertEqual(result.exit_code, 0)
 
-    def test_edit_not_exist(self):
-        runner = CliRunner()
-        result = runner.invoke(main, ['edit'], input='tester_test')
-        assert "Test 'tester_test' does not exist" in result.output
-        self.assertEqual(result.exit_code, 0)
+    # def test_edit_not_exist(self):
+    #     runner = CliRunner()
+    #     result = runner.invoke(main, ['edit'], input='tester_test')
+    #     assert "Test 'tester_test' does not exist" in result.output
+    #     self.assertEqual(result.exit_code, 0)
 
-    # figure out how to exit graph
-    def test_run_no_out(self):
-        runner = CliRunner()
-        result = runner.invoke(main, ['run'], input='test/n10')
-        runner.invoke(main, input='10')
-        self.assertEqual(result.exit_code, 0)
+    # # figure out how to exit graph
+    # def test_run_no_out(self):
+    #     runner = CliRunner()
+    #     result = runner.invoke(main, ['run'], input='test/n10')
+    #     runner.invoke(main, input='10')
+    #     self.assertEqual(result.exit_code, 0)
 
-    def test_run_out(self):
-        runner = CliRunner()
-        result = runner.invoke(main, ['run', '-o test'], input='test\n10')
-        self.assertEqual(result.exit_code, 0)
+    # def test_run_out(self):
+    #     runner = CliRunner()
+    #     result = runner.invoke(main, ['run', '-o test'], input='test\n10')
+    #     self.assertEqual(result.exit_code, 0)
 
     # def test_delete_notexist(self):
     #     runner = CliRunner()
