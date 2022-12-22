909613bdb2d543097deee07de310901d874abf1b
Tristin <tmille2004@gmail.com>
2022-12-22 13:52:06 -0600

    adds docstrings

--- cli/strategy_tester.py
+++ cli/strategy_tester.py
@@ -38,7 +38,7 @@ class Tester:
         self.indicators = indicators
         self.start_date = start_date
         self.end_date = end_date
-        self.data = self.retrieve_stock_data()
+        self.stock_data = self.retrieve_stock_data()
         self.num_of_shares = 0
         self.num_of_trades = 0
         self.last_buy_price = 0
@@ -46,9 +46,10 @@ class Tester:
         self.output += f"\nBuy Criteria: {self.buy_criteria}\nSell criteria: {self.sell_criteria}\n"
         self.output += f"Starting Balance: ${self.balance} | Running test on {self.stock}\n\n"
         self.graph_output = {'date': [start_date], 'balance': [self.balance]}
+        self.strategy = Strategy(indicators, self.stock_data)
 
     def run_test(self):
-        for index, row in self.data.iterrows():
+        for index, row in self.stock_data.iterrows():
             if self.balance != 0:
                 # look for buying
                 # check if each buy criteria is True and then buy if so
@@ -78,13 +79,13 @@ class Tester:
 
         # if you still own shares after all of it sell it all at latest price
         if self.num_of_shares > 0:
-            self.output += f"{row['time']} | Sold {round(self.num_of_shares, 1)} shares @ ${int(self.data.iloc[-1]['close'])}\n"
-            self.balance = self.num_of_shares* int(self.data.iloc[-1]['close'])
+            self.output += f"{row['time']} | Sold {round(self.num_of_shares, 1)} shares @ ${int(self.stock_data.iloc[-1]['close'])}\n"
+            self.balance = self.num_of_shares* int(self.stock_data.iloc[-1]['close'])
             self.output += f"Balance: ${int(self.balance)}\n"
             self.num_of_trades += 1
 
         self.output += f"Number of trades: {self.num_of_trades} | Percent gain: {round((self.balance/1000)*100, 2)}%"
-        self.output += self.buy_and_hold()
+        self.output += f"\n\nReturn if you just bought and held {self.stock} from {self.start_date} to {self.end_date}: {self.strategy.buy_and_hold()}%"
         
         return self.output, self.graph_output
     
@@ -92,63 +93,20 @@ class Tester:
         for criteria in all_criteria:
                 # possibly use a switch statement for the different criteria
                 if 'crossing' in criteria:
-                    check = self.crossing(row, criteria.split(' ', 1)[1])
+                    check = self.strategy.crossing(row, criteria.split(' ', 1)[1])
                     if check == False:
                         return False
                 elif 'divergence' in criteria:
-                    check = self.divergence(row, criteria.split(' ', 1)[1])
+                    check = self.strategy.divergence(row, criteria.split(' ', 1)[1])
                     if check == False:
                         return False
                 elif 'tp' or 'sl' in criteria:
-                    check = self.tp_sl(row, criteria.split(' '))
+                    check = self.strategy.tp_sl(row, criteria.split(' '), self.last_buy_price)
                     if check == False:
                         return False
 
         return True
 
-    def crossing(self, data, criteria):
-        # check the criteria/argument and if it is true return True
-        criteria = criteria.split()
-        # replace variables with their values
-        if criteria[2] in ['open', 'close', 'high', 'low']:
-            criteria[2] = int(data[criteria[2]])
-        if criteria[2] in self.indicators:
-            criteria[2] = int(data[criteria[2]])
-
-        if criteria[1] == '<':
-            return (int(data[criteria[0]]) < int(criteria[2]))
-        elif criteria[1] == '>':
-            return (int(data[criteria[0]]) > int(criteria[2]))
-
-    def divergence(self, data, criteria):
-        criteria = criteria.split()
-        # get the value 30 rows before the current one or none
-        index = self.data.loc[self.data['time'] == data['time']].index
-        if index <= int(criteria[2]):
-            return False
-
-        x_days_ago_data = self.data.iloc[index-int(criteria[2])]
-
-        if criteria[0] == 'bull':
-            # check if price is lower than it was 30 days ago but rsi is higher than it was 30 days ago
-            if int(data['close']) < int(x_days_ago_data['close']) and int(data[criteria[1]]) > int(x_days_ago_data[criteria[1]]):
-                return True
-        
-        if criteria[0] == 'bear':
-            if int(data['close']) > int(x_days_ago_data['close']) and int(data[criteria[1]]) < int(x_days_ago_data[criteria[1]]):
-                return True
-
-    def tp_sl(self, data, criteria):
-        # if sl or tp is hit sell/ return True
-        take_profit = int(criteria[1].replace('%', ''))
-        stop_loss = int(criteria[3].replace('%', ''))
-        # check both closing price and high price
-        if calc_percent_diff(data['close'], self.last_buy_price) >= take_profit or calc_percent_diff(data['high'], self.last_buy_price) >= take_profit:
-            return True
-        
-        elif calc_percent_diff(data['close'], self.last_buy_price) <= stop_loss or calc_percent_diff(data['low'], self.last_buy_price) <= stop_loss:
-            return True
-    
     def retrieve_stock_data(self):
         path = pathlib.Path(__file__).parent.resolve()
         with open(f"{path}/credentials.txt", 'r') as file:
@@ -161,13 +119,6 @@ class Tester:
 
         return data
 
-    def buy_and_hold(self):
-        start_price = self.data.iloc[0]['close']
-        end_price = self.data.iloc[-1]['close']
-
-        return f"\n\nReturn if you just bought and held {self.stock} from {self.start_date} to {self.end_date}: {round((end_price/start_price)*100, 2)}%"
-
-
 def calc_percent_diff(current, previous):
     if current == previous:
         return 0
@@ -175,3 +126,58 @@ def calc_percent_diff(current, previous):
         return ((current - previous) / previous) * 100.0
     except ZeroDivisionError:
         return 0
+
+
+class Strategy:
+    def __init__(self, indicators, stock_data):
+        self.indicators = indicators
+        self.stock_data = stock_data
+
+    def crossing(self, stock_data, criteria):
+        # check the criteria/argument and if it is true return True
+        criteria = criteria.split()
+        # replace variables with their values
+        if criteria[2] in ['open', 'close', 'high', 'low']:
+            criteria[2] = int(stock_data[criteria[2]])
+        if criteria[2] in self.indicators:
+            criteria[2] = int(stock_data[criteria[2]])
+
+        if criteria[1] == '<':
+            return (int(stock_data[criteria[0]]) < int(criteria[2]))
+        elif criteria[1] == '>':
+            return (int(stock_data[criteria[0]]) > int(criteria[2]))
+    
+    def tp_sl(self, stock_data, criteria, last_buy_price):
+        # if sl or tp is hit sell/ return True
+        take_profit = int(criteria[1].replace('%', ''))
+        stop_loss = int(criteria[3].replace('%', ''))
+        # check both closing price and high price
+        if calc_percent_diff(stock_data['close'], last_buy_price) >= take_profit or calc_percent_diff(stock_data['high'], last_buy_price) >= take_profit:
+            return True
+        
+        elif calc_percent_diff(stock_data['close'], last_buy_price) <= stop_loss or calc_percent_diff(stock_data['low'], last_buy_price) <= stop_loss:
+            return True
+    
+    def divergence(self, stock_data, criteria):
+        criteria = criteria.split()
+        # get the value 30 rows before the current one or none
+        index = self.stock_data.loc[self.stock_data['time'] == stock_data['time']].index
+        if index <= int(criteria[2]):
+            return False
+
+        x_days_ago_data = self.stock_data.iloc[index-int(criteria[2])]
+
+        if criteria[0] == 'bull':
+            # check if price is lower than it was 30 days ago but rsi is higher than it was 30 days ago
+            if int(stock_data['close']) < int(x_days_ago_data['close']) and int(stock_data[criteria[1]]) > int(x_days_ago_data[criteria[1]]):
+                return True
+        
+        if criteria[0] == 'bear':
+            if int(stock_data['close']) > int(x_days_ago_data['close']) and int(stock_data[criteria[1]]) < int(x_days_ago_data[criteria[1]]):
+                return True
+        
+    def buy_and_hold(self):
+        start_price = self.stock_data.iloc[0]['close']
+        end_price = self.stock_data.iloc[-1]['close']
+
+        return round((end_price/start_price)*100, 2)
