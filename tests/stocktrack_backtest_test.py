b6505e4ff75bcb1405ba8ab82484c6e30ff73821
Tristin <tmille2004@gmail.com>
2022-12-22 14:32:33 -0600

    updates tests

--- tests/stocktrack_backtest_test.py
+++ tests/stocktrack_backtest_test.py
@@ -4,6 +4,7 @@ from cli.stocktrack import main
 import pathlib
 import requests
 import json
+import os
 
 def get_path():
     return pathlib.Path(__file__).parent.resolve()
@@ -39,35 +40,52 @@ class StockTrackTests(unittest.TestCase):
     def setUp(self):
         self.runner = CliRunner()
         self.path = get_path()
+        self.test_name = 'test123'
+        self.output_name = 'test'
+    
+    def tearDown(self):
+        # Clean up any files or directories created during the tests
+        test_dir = get_test_dir()
+        if os.path.isfile(f"{self.path}/{self.test_name}.txt"):
+            os.remove(f"{self.path}/{self.test_name}.txt")
+        if os.path.isfile(f"{self.path}/{self.output_name}.txt"):
+            os.remove(f"{self.path}/{self.output_name}.txt")
 
     def test_dirs(self):
         result = self.runner.invoke(main, ['setdir'], input=f"{self.path}\n{self.path}")
         self.assertEqual(result.exit_code, 0)
 
     def test_create(self):
-        result = self.runner.invoke(main, ['create', 'test123'])
-        print(result.output, 'alsjfoiasfpsd-fj9sdfj9')
+        # test creating a new test
+        result = self.runner.invoke(main, ['create', self.test_name])
         self.assertEqual(result.exit_code, 0)
+        self.assertTrue(os.path.isfile(f"{self.path}/{self.test_name}.txt"))
+
+        # Test creating a test with a name that already exists
+        result = self.runner.invoke(main, ['create', self.test_name])
+        self.assertIn(f"Test '{self.test_name}' already exists", result.output)
+
+
 
     # figure out how to exit graph
-    def test_run_no_out(self):
-        result = self.runner.invoke(main, ['run'], input='test/n10')
-        self.runner.invoke(main, input='10')
-        self.assertEqual(result.exit_code, 0)
+    # def test_run_no_out(self):
+    #     result = self.runner.invoke(main, ['run'], input='test/n10')
+    #     self.runner.invoke(main, input='10')
+    #     self.assertEqual(result.exit_code, 0)
 
-    def test_run_out(self):
-        result = self.runner.invoke(main, ['run', '-o test'], input='test\n10')
-        self.assertEqual(result.exit_code, 0)
+    # def test_run_out(self):
+    #     result = self.runner.invoke(main, ['run', '-o test'], input='test\n10')
+    #     self.assertEqual(result.exit_code, 0)
 
-    def test_delete_notexist(self):
-        result = self.runner.invoke(main, ['delete'], input='not_exist')
-        assert "Error deleting not_exist" in result.output
-        self.assertEqual(result.exit_code, 0)
+    # def test_delete_notexist(self):
+    #     result = self.runner.invoke(main, ['delete'], input='not_exist')
+    #     assert "Error deleting not_exist" in result.output
+    #     self.assertEqual(result.exit_code, 0)
 
-    def test_delete_exist(self):
-        result = self.runner.invoke(main, ['delete'], input='test123')
-        assert "test123 Successfully Deleted" in result.output
-        self.assertEqual(result.exit_code, 0)
+    # def test_delete_exist(self):
+    #     result = self.runner.invoke(main, ['delete'], input='test123')
+    #     assert "test123 Successfully Deleted" in result.output
+    #     self.assertEqual(result.exit_code, 0)
 
 if __name__ == "__main__":
     unittest.main()
