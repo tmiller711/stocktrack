d7dcc8e2f821b0a7445af79cf37ae78197c15aec
Tristin <tmille2004@gmail.com>
2022-12-22 14:10:48 -0600

    adds simple backtest tests

--- tests/stocktrack_backtest_test.py
+++ tests/stocktrack_backtest_test.py
@@ -2,45 +2,73 @@ import unittest
 from click.testing import CliRunner
 from cli.stocktrack import main
 import pathlib
+import requests
+import json
+
+def get_path():
+    return pathlib.Path(__file__).parent.resolve()
+
+def check_token():
+    try:
+        with open(f"{get_path()}/credentials.txt", 'r') as file:
+            data = json.loads(file.read())
+            r = requests.get('http://127.0.0.1:8000/account/getaccount', headers={'Authorization': f"Bearer {data['access']}"})
+            if r.ok:
+                return True
+            else:
+                return False
+    except:
+        return False
+
+def get_test_dir():
+    try:
+        with open(f"{get_path()}/test_dir.txt", "r") as f:
+            return f.read().strip()
+    except:
+        return None
+
+def get_results_dir():
+    try:
+        with open(f"{get_path()}/results_dir.txt", "r") as f:
+            return f.read().strip()
+    except:
+        return None
+
 
 class StockTrackTests(unittest.TestCase):
-    pass
-    # def test_dirs(self):
-    #     runner = CliRunner()
-    #     path = pathlib.Path(__file__).parent.resolve()
-    #     print(path)
-    #     result = runner.invoke(main, ['setdir'], input=f"{path}\n{path}")
-    #     self.assertEqual(result.exit_code, 0)
+    def setUp(self):
+        self.runner = CliRunner()
 
-    # def test_create(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['create', 'test123'])
-    #     print(result.output)
-    #     self.assertEqual(result.exit_code, 0)
+    def test_dirs(self):
+        path = pathlib.Path(__file__).parent.resolve()
+        print(path)
+        result = self.runner.invoke(main, ['setdir'], input=f"{path}\n{path}")
+        self.assertEqual(result.exit_code, 0)
 
-    # # figure out how to exit graph
-    # def test_run_no_out(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['run'], input='test/n10')
-    #     runner.invoke(main, input='10')
-    #     self.assertEqual(result.exit_code, 0)
+    def test_create(self):
+        result = self.runner.invoke(main, ['create', 'test123'])
+        print(result.output)
+        self.assertEqual(result.exit_code, 0)
 
-    # def test_run_out(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['run', '-o test'], input='test\n10')
-    #     self.assertEqual(result.exit_code, 0)
+    # figure out how to exit graph
+    def test_run_no_out(self):
+        result = self.runner.invoke(main, ['run'], input='test/n10')
+        self.runner.invoke(main, input='10')
+        self.assertEqual(result.exit_code, 0)
 
-    # def test_delete_notexist(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['delete'], input='not_exist')
-    #     assert "Error deleting not_exist" in result.output
-    #     self.assertEqual(result.exit_code, 0)
+    def test_run_out(self):
+        result = self.runner.invoke(main, ['run', '-o test'], input='test\n10')
+        self.assertEqual(result.exit_code, 0)
 
-    # def test_delete_exist(self):
-    #     runner = CliRunner()
-    #     result = runner.invoke(main, ['delete'], input='test')
-    #     assert "test Successfully Deleted" in result.output
-    #     self.assertEqual(result.exit_code, 0)
+    def test_delete_notexist(self):
+        result = self.runner.invoke(main, ['delete'], input='not_exist')
+        assert "Error deleting not_exist" in result.output
+        self.assertEqual(result.exit_code, 0)
+
+    def test_delete_exist(self):
+        result = self.runner.invoke(main, ['delete'], input='test123')
+        assert "test123 Successfully Deleted" in result.output
+        self.assertEqual(result.exit_code, 0)
 
 if __name__ == "__main__":
     unittest.main()
